64cceeb4e1773ae468438fd787946de5b66b6c48
taslima97 <antora.taslima97@gmail.com>
2023-06-07 15:15:51 +0600

    use get method

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ app.use(express.json());
 
 
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p54mfnr.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,6 +27,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+const classCollection = client.db('artandcraftdb').collection('classes');
+
+
+app.get('/classes', async(req, res)=>{
+    const result = await classCollection.find().toArray();
+    res.send(result)
+})
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
